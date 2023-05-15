bc1ba12a232ee9ae02100c3bb8e1552cbf844d6f
Julian Cardone <juliancardone1@gmail.com>
2023-05-15 15:41:11 -0400

    first commit

---
+++ backend/src/main/java/com/jc/clGPT/ClGptApplication.java
@@ -0,0 +1,13 @@
+package com.jc.clGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClGptApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jc/clGPT/ClGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.clGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
