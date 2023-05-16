edefb209a611c3921fa83675c8b0f6cd97997bc8
Julian Cardone <juliancardone1@gmail.com>
2023-05-16 14:44:00 -0400

    whitelist routes

--- backend/src/main/java/com/jc/clGPT/auth/AuthenticationService.java
+++ backend/src/main/java/com/jc/clGPT/auth/AuthenticationService.java
@@ -1,19 +1,52 @@
 package com.jc.clGPT.auth;
 
+import com.jc.clGPT.config.JwtService;
+import com.jc.clGPT.user.Role;
+import com.jc.clGPT.user.User;
+import com.jc.clGPT.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+                );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();     //todo exceptions
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
 }
